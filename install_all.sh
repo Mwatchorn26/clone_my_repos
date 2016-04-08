f4f8fd71ec2f622758c254d8ba24b583c287fee4
Michael Watchorn <mwatchorn26@gmail.com>
2016-04-08 14:29:30 -0400

    Update install_all.sh
    moved cloned repos that I want out of their parent folders.

--- install_all.sh
+++ install_all.sh
@@ -7,13 +7,20 @@ then
 else
   echo "Installing TEI Custom modules..."
   echo "Update Module List..."
-  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -u all
+  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/custom/addons/OCA/,/opt/odoo/odoo-server/addons/ -u all
   echo "Install custom modules..."
-  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -i project_issue_service
-  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -i project_serial_numbers
-  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -i crm_eto
-  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -i Odoo-App/web_list_view_sticky
-  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -i l10n-canada/l10n_ca_toponyms
+  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/custom/addons/OCA/,/opt/odoo/odoo-server/addons/ -i project_issue_service
+  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/custom/addons/OCA/,/opt/odoo/odoo-server/addons/ -i project_serial_numbers
+  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/custom/addons/OCA/,/opt/odoo/odoo-server/addons/ -i crm_eto
+  mv /opt/odoo/custom/addons/Odoo-App/web_list_view_sticky /opt/odoo/custom/addons/web_list_view_sticky
+  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/custom/addons/OCA/,/opt/odoo/odoo-server/addons/ -i web_list_view_sticky
+  mv /opt/odoo/custom/addons/l10n-canada/l10n_ca_toponyms /opt/odoo/custom/addons/l10n_ca_toponyms
+  python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/custom/addons/OCA/,/opt/odoo/odoo-server/addons/ -i l10n_ca_toponyms
+  
+  #Uncomment these if going on a production server and you don't want users accessing the database manager:
+  #mv /opt/odoo/custom/addons/tkobr-addons/tko_hide_login_manage_databases_link  /opt/odoo/custom/addons/tko_hide_login_manage_databases_link 
+  #python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/custom/addons/OCA/,/opt/odoo/odoo-server/addons/ -i tko_hide_login_manage_databases_link
+  
   #,mrp_shopfloor_terminal
   #auth_ldap_rewrite
   printf "\n\n************** DONE **************\n"
