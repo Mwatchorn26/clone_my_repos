12791a1cc378291d6e7e38f92db1c9a645fcf3e3
Michael Watchorn <mwatchorn26@gmail.com>
2016-04-08 11:50:20 -0400

    Update install_all.sh
    changed auth_ldaps for auth_ldap_rewrite.Rewrite works, just don't install auth_ldap first! (but DO make sure auth_crypt is uninstalled!)

--- install_all.sh
+++ install_all.sh
@@ -10,7 +10,8 @@ else
   python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -u all
   echo "Install custom modules..."
   python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -i \
-  auth_ldaps,project_issue_service,project_serial_numbers,crm_eto,Odoo-App/web_list_view_sticky,l10n-canada/l10n_ca_toponyms
+  project_issue_service,project_serial_numbers,crm_eto,Odoo-App/web_list_view_sticky,l10n-canada/l10n_ca_toponyms
   #,mrp_shopfloor_terminal
+  #auth_ldap_rewrite
   printf "\n\n************** DONE **************\n"
 fi
