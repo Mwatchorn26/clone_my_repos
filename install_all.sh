1b45cbf15d54105a8171a45707c32f249abf0326
Michael Watchorn <mwatchorn26@gmail.com>
2016-04-08 09:38:39 -0400

    Update install_all.sh
    Moved "Done" to inside the ELSE case, where the database was given.

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-if [ -z "$1" ] then;
+if [ -z "$1" ]
+then
   echo "Missing argument: Database name to install the modules in is missing."
   echo "Usage call ./install_all.sh name_of_db"
 else
@@ -9,7 +10,7 @@ else
   python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -u all
   echo "Install custom modules..."
   python /opt/odoo/odoo-server/odoo.py -d $1 --stop-after-init --addons-path /opt/odoo/custom/addons/,/opt/odoo/odoo-server/addons/ -i \
-  auth_ldaps,project_issue_service,project_serial_numbers,crm_eto,Odoo-App/web_list_view_sticky,l10n-canada/l10n_ca_toponyms 
+  auth_ldaps,project_issue_service,project_serial_numbers,crm_eto,Odoo-App/web_list_view_sticky,l10n-canada/l10n_ca_toponyms
   #,mrp_shopfloor_terminal
+  printf "\n\n************** DONE **************\n"
 fi
-printf "\n\n************** DONE **************\n"
