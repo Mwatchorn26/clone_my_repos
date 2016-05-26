b820d625293b855c96c86649661b3558c7fb0c40
Michael Watchorn <mwatchorn26@gmail.com>
2016-05-26 15:33:38 -0400

    Update clone_all.sh
    changed path to custom addons for a variable since it can change.

--- clone_all.sh
+++ clone_all.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #echo "Run in interactive mode?(y/n)"
 #read interactive
-if [ $1="interactive" ] 
+if [ $1="interactive" ]
 then
   set -x
   trap read debug
@@ -13,15 +13,17 @@ fi
 
 if [ -d /opt/odoo/addons/ ]; then
   #PROD and TEST servers:
-  if [ -d /opt/odoo/addons/custom ]; then
+  $CUSTOM_ADDONS_PATH = "/opt/odoo/addons/custom"
+  if [ -d  $CUSTOM_ADDONS_PATH ]; then
     echo "custom folder exists"
   else
     echo "creating custom folder"
-    mkdir /opt/odoo/addons/custom
-    cd /opt/odoo/addons/custom
+    mkdir $CUSTOM_ADDONS_PATH
+    cd $CUSTOM_ADDONS_PATH
   fi
 else #Dev server install:
-  cd /opt/odoo/custom/addons/
+  $CUSTOM_ADDONS_PATH = "/opt/odoo/custom/addons/"
+  cd $CUSTOM_ADDONS_PATH
 fi
 
 echo "Cloning TEI Custom modules..."
@@ -36,42 +38,43 @@ echo ""
 echo "Cloning Additional Required modules..."
 echo ""
 
-if [ ! -d /opt/odoo/custom/addons/Akretion ] 
+if [ ! -d $CUSTOM_ADDONS_PATH/Akretion ]
 then
-  mkdir /opt/odoo/custom/addons/Akretion
+  mkdir $CUSTOM_ADDONS_PATH/Akretion
 fi
-cd /opt/odoo/custom/addons/Akretion
+cd $CUSTOM_ADDONS_PATH/Akretion
 git clone -b 8.0 --depth=1 https://github.com/Mwatchorn26/odoo-usability.git
+
 #you need to remove this account_move_line_start_end_dates_xls it creates a problem. There's a bug with it.
 #root@OdooVM1:/opt/odoo/custom/addons/Akretion/odoo-usability# rm -rf account_move_line_start_end_dates_xls/
-rm -rf /opt/odoo/custom/addons/Akretion/odoo-usability/account_move_line_start_end_dates_xls
+rm -rf /$CUSTOM_ADDONS_PATH/opt/odoo/custom/addons/Akretion/odoo-usability/account_move_line_start_end_dates_xls
 
-if [ ! -d /opt/odoo/custom/addons/Elghard ] 
+if [ ! -d $CUSTOM_ADDONS_PATH/Elghard ]
 then
-  mkdir /opt/odoo/custom/addons/Elghard
+  mkdir $CUSTOM_ADDONS_PATH/opt/odoo/custom/addons/Elghard
 fi
-cd /opt/odoo/custom/addons/Elghard
+cd $CUSTOM_ADDONS_PATH/Elghard
 echo "Get the Repository that holds Cloning Web List View Fixed Table Header module."
 git clone -b 8.0  --depth=1 https://github.com/Elghard/Odoo-App
 
-if [ ! -d /opt/odoo/custom/addons/thinkopensolutions ] 
+if [ ! -d $CUSTOM_ADDONS_PATH/thinkopensolutions ]
 then
-  mkdir /opt/odoo/custom/addons/thinkopensolutions
+  mkdir $CUSTOM_ADDONS_PATH/thinkopensolutions
 fi
-cd /opt/odoo/custom/addons/thinkopensolutions
+cd $CUSTOM_ADDONS_PATH/thinkopensolutions
 echo "Get the Repository that holds Hide Login Manage Databases Link module."
 git clone -b 8.0  --depth=1 https://github.com/thinkopensolutions/tkobr-addons.git
 
 #Prep for all the OCA (Odoo Community Association) repositories and modules
-if [ ! -d /opt/odoo/custom/addons/OCA ] 
+if [ ! -d $CUSTOM_ADDONS_PATH/opt/odoo/custom/addons/OCA ]
 then
-  mkdir /opt/odoo/custom/addons/OCA
+  mkdir $CUSTOM_ADDONS_PATH/OCA
 fi
-cd /opt/odoo/custom/addons/OCA
+cd $CUSTOM_ADDONS_PATH/OCA
 
 echo "Get Canadian Provinces"
 git clone -b 8.0 --depth=1 https://github.com/OCA/l10n-canada.git
-  
+
 echo "Get OCA Manufacturing  Repository:"
 git clone -b 8.0 --depth=1 https://github.com/OCA/manufacture.git
 
@@ -95,11 +98,11 @@ git clone -b 8.0 --depth=1 https://github.com/OCA/partner-contact.git
 
 
 
-if [ ! -d /opt/odoo/custom/addons/dreispt ] 
+if [ ! -d $CUSTOM_ADDONS_PATH/dreispt ]
 then
-  mkdir /opt/odoo/custom/addons/dreispt
+  mkdir $CUSTOM_ADDONS_PATH/dreispt
 fi
-cd /opt/odoo/custom/addons/dreispt
+cd $CUSTOM_ADDONS_PATH/dreispt
 git clone -b 8.0 --depth=1 https://github.com/dreispt/odoo-addons.git
 
 #echo "NOW YOU NEED TO PRESS 'Update Modules List' IN THE SETTINGS MENU"
