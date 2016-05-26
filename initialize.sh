c1a96f2a030236c13438fcabf8a3dee11334a166
Michael Watchorn <mwatchorn26@gmail.com>
2016-05-26 14:25:22 -0400

    Create initialize.sh
    moved root startup portion from original script. Turns out we need to call the other script as the odoo user.

---
+++ initialize.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+echo "Run in interactive mode?(y/n)"
+read interactive
+if [ interactive="y" ] 
+then
+  set -x
+  trap read debug
+else
+  echo ""
+  echo "Proceeding with without further interaction."
+  sleep 1
+fi
+#install Bazaar, where some of the files still reside
+#Bazaar should already be installed by the odoo install script.
+#sudo apt-get install bzr
+
+#switch to the Odoo user for the rest of the commands
+#if [ $USER -eq "odoo" ] 
+printf "\n\nSwitching to user: odoo.\n\nPlease enter your password."
+#sudo su - odoo -s /bin/bash
+su -c "clone_all.sh" -m "odoo" 
+#fi
+
+exit
